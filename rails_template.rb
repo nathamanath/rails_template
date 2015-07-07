3898bd600aac47b139b325ff4739e82ba61719fd
NathanG <nathangoddard@me.com>
2015-07-07 22:49:21 +0100

    thats handy

--- rails_template.rb
+++ rails_template.rb
@@ -1,3 +1,13 @@
+remove_file 'Gemfile'
+run 'touch Gemfile'
+
+add_source 'https://rubygems.org'
+
+# Add some gems
+gem 'rails'
+gem 'pg'
+gem 'sass-rails'
+gem 'uglifier'
 gem 'puma'
 gem 'bourbon'
 gem 'slim-rails'
@@ -5,6 +15,8 @@ gem 'slim-rails'
 gem_group :development, :test do
   gem 'byebug'
   gem 'rspec-rails'
+  gem 'web-console'
+  gem 'spring'
   gem 'pry'
   gem 'letter_opener'
   gem 'better_errors'
@@ -21,11 +33,16 @@ gem_group :production, :staging do
   gem 'rails_12factor'
 end
 
+# set ruby version
+version = `ruby --version`.match(/\d.\d.\d/)[0]
+run %Q{ sed -i "2i ruby '#{version}'" Gemfile }
+
+# Environment variables
 run 'echo ".env" >> .gitignore'
 run 'touch .env.sample'
 run 'touch .env'
 
-
+# Nice README
 run 'rm README.rdoc'
 run "echo '# #{@app_name.titleize}\n' > README.md"
 
@@ -33,14 +50,18 @@ run 'bundle:install'
 
 generate 'rspec:install'
 
+# SASS
 sass = <<-SASS
 @import 'bourbon'
 SASS
 
+# TODO:
 # update application.rb
 # selenium
-run "rm app/assets/stylesheets/application.css"
-run %Q{ echo "#{sass}" > app/assets/stylesheets/application.css.sass }
+# create staging.rb
+
+remove_file 'app/assets/stylesheets/application.css'
+create_file 'app/assets/stylesheets/application.css.sass', sass
 
 # sass directories
 sass_paths = %W{base components helpers ie layout pages themes vendor}
@@ -53,18 +74,13 @@ sass_paths.each do |path|
   run %Q{ echo "@import '#{file}'" >> app/assets/stylesheets/application.css.sass }
 end
 
-
 run 'mkdir spec/support'
 
 # factory girl
-girl = File.read File.expand_path('../spec/support/factory_girl.rb', __FILE__)
-run "echo '#{girl}' > spec/support/factory_girl.rb"
-
+copy_file File.expand_path('../spec/support/factory_girl.rb', __FILE__), 'spec/support/factory_girl.rb'
 
 # database cleaner
-cleaner = File.read File.expand_path('../spec/support/database_cleaner.rb', __FILE__)
-run "echo '#{cleaner}' > spec/support/database_cleaner.rb"
-
+copy_file File.expand_path('../spec/support/database_cleaner.rb', __FILE__), 'spec/support/database_cleaner.rb'
 
 # rspec
 rspec = <<-RSPEC
@@ -74,25 +90,40 @@ require 'pry'
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
 RSPEC
 
-spec_helper_path = 'spec/spec_helper.rb'
+prepend_to_file 'spec/spec_helper.rb', rspec
 
-content = File.read spec_helper_path
-
-File.open spec_helper_path, 'w+' do |f|
-  f.puts rspec
-  f.puts content
-end
 
 # letter opener
-opener = <<-OPENER
+insert_into_file 'config/environments/development.rb', after: "Rails.application.configure do\n" do <<-OPENER
   config.action_mailer.delivery_method = :letter_opener
 OPENER
+end
 
-run "sed -i '2i #{opener}' config/environments/development.rb"
+# staging environment
+run 'cp config/environments/production.rb config/environments/staging.rb'
+gsub_file 'config/environments/staging.rb', /:debug/, ':warn'
+
+capitalized = @app_name.upcase
+
+staging = <<-YML
+
+staging:
+  <<: *default
+  database: #{@app_name}_staging
+  username: <%= ENV['#{capitalized}_DATABASE_USERNAME'] %>
+  password: <%= ENV['#{capitalized}_DATABASE_PASSWORD'] %>
+
+YML
+
+append_to_file 'config/database.yml', staging
+run 'cp config/database.yml config/database.yml.sample'
+append_to_file '.gitignore', 'config/database.yml'
 
 rake 'db:setup'
+rake 'db:test:prepar'
 
 git :init
 git add: '.'
 git commit: '-a -m initial commit'
+git checkout: '-b develop'
 
