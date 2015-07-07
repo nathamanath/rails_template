1334c148899ffa8b5d8f85b72398de3fcbd55668
NathanG <nathangoddard@me.com>
2015-07-07 18:59:48 +0100

    some config

--- rails_template.rb
+++ rails_template.rb
@@ -21,15 +21,10 @@ gem_group :production, :staging do
   gem 'rails_12factor'
 end
 
-sass = <<-SASS
-@import 'bourbon'
-
-SASS
-
 run 'echo ".env" >> .gitignore'
+run 'touch .env.sample'
+run 'touch .env'
 
-run "rm app/assets/stylesheets/application.css"
-run "echo '#{sass} > app/assets/stylesheets/application.sass'"
 
 run 'rm README.rdoc'
 run "echo '# #{@app_name.titleize}\n' > README.md"
@@ -37,6 +32,63 @@ run "echo '# #{@app_name.titleize}\n' > README.md"
 run 'bundle:install'
 
 generate 'rspec:install'
+
+sass = <<-SASS
+@import 'bourbon'
+SASS
+
+# update application.rb
+# selenium
+run "rm app/assets/stylesheets/application.css"
+run %Q{ echo "#{sass}" > app/assets/stylesheets/application.css.sass }
+
+# sass directories
+sass_paths = %W{base components helpers ie layout pages themes vendor}
+
+sass_paths.each do |path|
+  file = "#{path}/#{path}"
+
+  run "mkdir app/assets/stylesheets/#{path}"
+  run "touch app/assets/stylesheets/#{file}.sass"
+  run %Q{ echo "@import '#{file}'" >> app/assets/stylesheets/application.css.sass }
+end
+
+
+run 'mkdir spec/support'
+
+# factory girl
+girl = File.read File.expand_path('../spec/support/factory_girl.rb', __FILE__)
+run "echo '#{girl}' > spec/support/factory_girl.rb"
+
+
+# database cleaner
+cleaner = File.read File.expand_path('../spec/support/database_cleaner.rb', __FILE__)
+run "echo '#{cleaner}' > spec/support/database_cleaner.rb"
+
+
+# rspec
+rspec = <<-RSPEC
+require 'pry'
+
+# Require support directory
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each { |f| require f }
+RSPEC
+
+spec_helper_path = 'spec/spec_helper.rb'
+
+content = File.read spec_helper_path
+
+File.open spec_helper_path, 'w+' do |f|
+  f.puts rspec
+  f.puts content
+end
+
+# letter opener
+opener = <<-OPENER
+  config.action_mailer.delivery_method = :letter_opener
+OPENER
+
+run "sed -i '2i #{opener}' config/environments/development.rb"
 
 rake 'db:setup'
 
---
+++ spec/support/factory_girl.rb
@@ -0,0 +1,10 @@
+require 'factory_girl'
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    FactoryGirl.lint
+  end
+end
+
